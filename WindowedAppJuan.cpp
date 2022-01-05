91945cec25b34c55694489863dc1cc1910d22bc5
Tomasz <tomigunpl@icloud.com>
2022-01-05 08:51:48 +0100

    First update
    Added dark theme, menu with option with hyperlink to my site on github

--- WindowedAppJuan.cpp
+++ WindowedAppJuan.cpp
@@ -2,15 +2,31 @@
 
 #include <vcl.h>
 #pragma hdrstop
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
+#include <windows.h>
+#include <shellapi.h>
+#include <winsock2.h>
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <locale>
+#pragma comment(lib,"ws2_32.lib")
 
 #include "WindowedAppJuan.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
-TForm1 *Form1;
+TWindow1 *Window1;
 //---------------------------------------------------------------------------
-__fastcall TForm1::TForm1(TComponent* Owner)
+__fastcall TWindow1::TWindow1(TComponent* Owner)
         : TForm(Owner)
 {
+}
+//---------------------------------------------------------------------------
+void __fastcall TWindow1::Pr1Click(TObject *Sender)
+{
+        ShellExecute(NULL, "open", "https://github.com/TomaszGorecki1", NULL, NULL, SW_SHOWNORMAL); // By clicking on button it redirect you to my github page
 }
 //---------------------------------------------------------------------------
